f0c28ee0b6ff847fd7a70fecc7e87c367f5e9334
pankajbariPB <pankajbari13official.com>
2025-01-17 09:44:19 +0530

    Docstring added

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Project Title: Student Data Management and Analysis
+ 
+Description:
+This project is designed as part of my Python learning journey. Its goal is to collect, manage, and analyze data from students. 
+The project will allow me to practice key concepts such as data collection, processing, and analysis. 
+It will also include features to generate personalized results and insights for the students based on their data.
+
+"""
+
