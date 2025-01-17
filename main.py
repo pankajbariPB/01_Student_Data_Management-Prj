f83d732882e110abd73afc712923dbdf7484265f
pankajbariPB <pankajbari13official.com>
2025-01-17 09:55:29 +0530

    Features of project added

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ This project is designed as part of my Python learning journey. Its goal is to c
 The project will allow me to practice key concepts such as data collection, processing, and analysis. 
 It will also include features to generate personalized results and insights for the students based on their data.
 
+1. add student details (name, age score in each sub.).
+2. view all student and their details.
+3. update and delete.
+4. Analyzing data finding top scorer in all sub, and in each sub.
 """
 
